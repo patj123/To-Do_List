bcaabbef07054a3866af4e18a250f77e8bb9e4e0
Patrick Gibbs <patrickjkgibbs@gmail.com>
2024-03-07 14:11:41 -0800

    Adding more to local storage

--- to-do.js
+++ to-do.js
@@ -1,10 +1,18 @@
+//const toDoList2 = document.getElementById('to-do-list');
+ const saveToDos = JSON.parse(localStorage.getItem("todos")) || [];
 
 document.addEventListener("DOMContentLoaded", function() {
+
     const newToDoForm = document.getElementById("newTodoForm");
-    const toDoList = document.getElementById('to-do-list')
-    const saveToDos = [];
+    const toDoList = document.getElementById('to-do-list');
     //submit function
     newToDoForm.addEventListener("submit", function (event) {
+        for (let i = 0; i < saveToDos.length; i++) {
+        let newToDo = document.createElement("li");
+        newToDo.innerText = saveToDos[i].task;
+        newToDo.isCompleted = saveToDos[i].isCompleted ? true : false
+        toDoList.appendChild(newToDo);
+    }
         let newToDo = document.createElement("li");
         newToDo.innerText = document.getElementById("task").value;
         // console.log(newToDo.innerText);
@@ -18,16 +26,24 @@ document.addEventListener("DOMContentLoaded", function() {
     });
     toDoList.addEventListener("click", function(event) {
         const targetTag = event.target.tagName.toLowerCase();
+        let clickedListItem = event.target
         if (targetTag === 'li') {
             console.log(event.target.style.textDecoration);
             if (event.target.style.textDecoration === "" || event.target.style.textDecoration === "none") {
                event.target.style.textDecoration = "line-through"
             } else if (event.target.style.textDecoration === "line-through"){
                 event.target.style.textDecoration = "none";
+                console.log(localStorage.getItem('todos').parse);
             }   
             
         } else if (targetTag === 'button') {
             event.target.parentElement.remove();
+        }
+        for (let i in saveToDos) {
+            if (saveToDos[i].task === event.target.innerText) {
+                saveToDos[i].isCompleted = !saveToDos[i].isCompleted
+                localStorage.setItem('todos', JSON.stringify(saveToDos))
+            }
         }
     })
 
